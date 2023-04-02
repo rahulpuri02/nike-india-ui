38068ad83bca702b72679e4f5b783ea8ee6e38b5
Rahul Puri <rahulxpuri@gmail.com>
2023-04-02 06:40:15 +0530

    working on product-details page

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,3 +1,4 @@
+import ProductCard from '@/components/ProductCard'
 import Wrapper from '@/components/Wrapper'
 import React from 'react'
 
@@ -8,7 +9,24 @@ const Category = () => {
   return (
     <div className='w-full md:py-20'>
     <Wrapper>
-      
+    <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+                    <h2 className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+                    Running Shoes
+                    </h2>
+                </div>
+        
+        {/*Product Grid */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </div>
+                
     </Wrapper>
     </div>
   )
